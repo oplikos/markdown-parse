f575f0091c4cc20ba0ab97d21c51fe4c850fa9cb
sarkis bouzikian <44330982+oplikos@users.noreply.github.com>
2022-01-19 15:57:53 -0800

    Update MarkdownParse.java
    copied of the same code which small changes but completely working at a different terminal to double-check if its something at my end

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,5 @@
 
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,9 +9,7 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-
+        
         int currentIndex = 0, nextOpenBracket = 0, nextCloseBracket = 0, openParen = 0, closeParen = 0;
         while (currentIndex < markdown.length()) {
             nextOpenBracket = markdown.indexOf("[", currentIndex);
@@ -32,16 +31,12 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen  , closeParen));
             currentIndex = closeParen + 1;
         }
-        //toReturn.add("hello");
         return toReturn;
-
     }
-
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
-        //System.out.println("hello");
         System.out.println(links);
     }
 }
