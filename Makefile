00ea7426c25a4d08e0156d9d79573f7474aa833a
sarkis bouzikian <44330982+oplikos@users.noreply.github.com>
2022-02-24 01:36:49 -0800

    changes renam and setting up test

--- Makefile
+++ Makefile
@@ -1,13 +1,9 @@
 CLASSPATH = lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar:lib/commonmark-0.18.1.jar:.
-
 MarkdownParseTest.class: MarkdownParseTest.java MarkdownParse.class
 	javac -g -cp $(CLASSPATH) MarkdownParseTest.java
-
 MarkdownParse.class: MarkdownParse.java
 	javac -g -cp $(CLASSPATH) MarkdownParse.java
-
 test: MarkdownParseTest.class
 	java -cp $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseTest 
-
 debug-test: MarkdownParseTest.class
-    jdb -classpath $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseTest
+	jdb -classpath $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseTest
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,21 +12,23 @@ public class MarkdownParseTest {
         Path fileName = Path.of("test-file.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
-       
+
     }
-    
+
     @Test
     public void testOneLinkMethod() throws IOException {
         Path fileName = Path.of("markdown-new-test.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
     }
+
     @Test
     public void testTwoLinkMethod() throws IOException {
         Path fileName = Path.of("markdown-test-two.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
     }
+
     @Test
     public void testThreeLinkMethod() throws IOException {
         Path fileName = Path.of("markdown-test-three.md");
@@ -34,43 +36,66 @@ public class MarkdownParseTest {
         assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
     }
 
-    //Tests from Joe's new repos
+    // Tests from Joe's new repos
     @Test
     public void joesNewTestTwo() throws IOException {
-        assertEquals(List.of("https://something.com", "some-page.html"), 
-            MarkdownParse.getLinks(getContents("test-file2.md")));
+        assertEquals(List.of("https://something.com", "some-page.html"),
+                MarkdownParse.getLinks(getContents("test-file2.md")));
     }
+
     @Test
     public void joesNewTestThree() throws IOException {
-        assertEquals(List.of(), 
-            MarkdownParse.getLinks(getContents("test-file3.md")));
+        assertEquals(List.of(),
+                MarkdownParse.getLinks(getContents("test-file3.md")));
     }
+
     @Test
     public void joesNewTestFour() throws IOException {
-        assertEquals(new ArrayList<String>(), 
-            MarkdownParse.getLinks(getContents("test-file4.md")));
+        assertEquals(new ArrayList<String>(),
+                MarkdownParse.getLinks(getContents("test-file4.md")));
     }
+
     @Test
     public void joesNewTestFive() throws IOException {
-        assertEquals(List.of("page.com"), 
-            MarkdownParse.getLinks(getContents("test-file5.md")));
+        assertEquals(List.of("page.com"),
+                MarkdownParse.getLinks(getContents("test-file5.md")));
     }
+
     @Test
     public void joesNewTestSix() throws IOException {
-        assertEquals(List.of("page.com"), 
-            MarkdownParse.getLinks(getContents("test-file6.md")));
+        assertEquals(List.of("page.com"),
+                MarkdownParse.getLinks(getContents("test-file6.md")));
     }
+
     @Test
     public void joesNewTestSeven() throws IOException {
-        assertEquals(new ArrayList<String>(), 
-            MarkdownParse.getLinks(getContents("test-file7.md")));
+        assertEquals(new ArrayList<String>(),
+                MarkdownParse.getLinks(getContents("test-file7.md")));
     }
+
     @Test
     public void joesNewTestEight() throws IOException {
-        assertEquals(new ArrayList<String>(), 
-            MarkdownParse.getLinks(getContents("test-file8.md")));
+        assertEquals(new ArrayList<String>(),
+                MarkdownParse.getLinks(getContents("test-file8.md")));
     }
-    
+
+    @Test
+    public void snippit1() throws IOException {
+        assertEquals(List.of("url.com", "google.com", "google.com", "ucsd.edu"),
+                MarkdownParse.getLinks(getContents("Snippet1.md")));
+    }
+
+    @Test
+    public void snippit2() throws IOException {
+        assertEquals(List.of("a.com", "b.com", "a.com", "example.com"),
+                MarkdownParse.getLinks(getContents("Snippet2.md")));
+    }
+
+    @Test
+    public void snippit3() throws IOException {
+        assertEquals(List.of("page.com"),
+                MarkdownParse.getLinks(getContents("Snippet3.md")));
+    }
 
     public static String getContents(String filePath) throws IOException {
         Path fileName = Path.of(filePath);
